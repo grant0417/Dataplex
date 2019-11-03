7c2ccf9ed5c0038bc1e10b9c4eacd60816c5761f
Bryan Triana <bryantrianarueda@gmail.com>
2019-11-03 00:54:53 -0400

    Added basic iterators for static array

--- include/StaticArray.hpp
+++ include/StaticArray.hpp
@@ -8,6 +8,7 @@ http://inversepalindrome.com
 #pragma once
 
 #include <cstddef>
+#include <iterator>
 
 
 namespace Dataplex
@@ -18,21 +19,84 @@ namespace Dataplex
     public:
         StaticArray();
 
+        T* begin();
+        const T* begin() const;
+
+        T* end();
+        const T* end() const;
+
+        T& operator[](std::size_t index);
+        const T& operator[](std::size_t index) const;
+
         std::size_t size() const;
+        bool is_empty() const;
 
     private:
+        T _array[N];
         std::size_t _size;
     };
 }
 
 template<typename T, std::size_t N>
 Dataplex::StaticArray<T, N>::StaticArray() :
-    size(N)
+    _size(N)
 { 
+}
+
+template<typename T, std::size_t N>
+T* Dataplex::StaticArray<T, N>::begin()
+{
+    return _array;
+}
+
+template<typename T, std::size_t N>
+const T* Dataplex::StaticArray<T, N>::begin() const
+{
+    return _array;
+}
+
+template<typename T, std::size_t N>
+T* Dataplex::StaticArray<T, N>::end()
+{
+    return _array + N;
+}
+
+template<typename T, std::size_t N>
+const T* Dataplex::StaticArray<T, N>::end() const
+{
+    return _array + N;
+}
+
+template<typename T, std::size_t N>
+T& Dataplex::StaticArray<T, N>::operator[](std::size_t index)
+{
+    if (index >= _size)
+    {
+        throw std::out_of_range("Index position out of range!");
+    }
+
+    return _array[index];
+}
+
+template<typename T, std::size_t N>
+const T& Dataplex::StaticArray<T, N>::operator[](std::size_t index) const
+{
+    if (index >= _size)
+    {
+        throw std::out_of_range("Index position out of range!");
+    }
+
+    return _array[index];
 }
 
 template<typename T, std::size_t N>
 std::size_t Dataplex::StaticArray<T, N>::size() const
 {
-    return _size
+    return _size;
+}
+
+template<typename T, std::size_t N>
+bool Dataplex::StaticArray<T, N>::is_empty() const
+{
+    return _size == 0;
 }
